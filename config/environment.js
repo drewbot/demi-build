429fa0afa8dd6014a69539f45adcd3e7afa3a815
Drew Botka <hellodrewbot@gmail.com>
2015-01-30 14:58:46 -0500

    new build

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'demi-app',
     environment: environment,
-    baseURL: 'demi-app',
+    baseURL: 'demi-build',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
